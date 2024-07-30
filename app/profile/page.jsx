00817fe70c8c0f6a685de291538ec7d32b93cc70
saqibrazzaq <saqibrazzaq@gmail.com>
2024-07-31 00:10:07 +0500

    env variables added

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -8,7 +8,6 @@ import Profile from "@components/Profile";
 const MyProfile = () => {
   const router = useRouter();
   const { data: session } = useSession();
-
   const [posts, setPosts] = useState([]);
 
   const handleEdit = (post) => {
